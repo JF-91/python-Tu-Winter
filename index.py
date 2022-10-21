7a8a5fe92e8e3b7ecea969e46997c0013407d9b1
JF-91 <johnf91@protonmail.com>
2022-10-21 12:24:36 +0200

    second  update

--- index.py
+++ index.py
@@ -1,23 +1,28 @@
 
 
-x= {1,23,6,34534,76,12,5,2323,4,9}
+x= [1,23,6,34534,76,12,5,2323,4,9]
 
 
 
 def maxArray(n):
 
-    for i in range(len(x)-1):
-        for j in range(len(x)):
-            if(n[i] >= n[i+1]):
-                temp = 0
-                n[i]= n[i+1]
-                n[i+1] = temp
+    for i in range(len(n)):
+        for j in range(len(n)-i-1):
+            if(n[j] >= n[j+1]):
+                temp = n[j]
+                n[j]= n[j+1]
+                n[j+1] = temp
 
-
-
+    
 
 
 maxArray(x)
+print(x[-1])
+
+
+
+
+
 
 
 
